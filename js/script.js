13dbddce9b11a926ef9f1f33f3c11013fd5f84b9
nehemia2 <149094739+nehemia2@users.noreply.github.com>
2023-10-30 19:05:07 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+pindah= 1;
+function ga(){
+    if(pindah == 1){
+        Btn.style.top=400;
+        Btn.style.left=300;
+        pindah=2;
+        }
+        else if(pindah == 2){
+            Btn.style.top=400;
+            Btn.style.left=70;
+            pindah=3;
+            }
+            else if(pindah== 3){
+                Btn.style.top=500;
+                Btn.style.left=100;
+                pindah=4;
+                }
+                
+}
